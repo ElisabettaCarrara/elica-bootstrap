e8c9399f5548afadb408814ec0bbf1ab8056257a
Elisabetta Carrara <146635386+elisabettac77@users.noreply.github.com>
2025-08-23 20:49:35 +0200

    included code to output custom header
    included code to output custom header

--- header.php
+++ header.php
@@ -67,6 +67,18 @@ $header_bg_color = get_theme_mod( 'elica_bootstrap_header_bg_color', '#ffffff' )
 			?>
 		</nav>
 
+		<!-- Custom Header Image Output -->
+		<?php if ( get_header_image() ) : ?>
+			<div class="custom-header-image">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+					<img src="<?php header_image(); ?>"
+						 width="<?php echo esc_attr( get_custom_header()->width ); ?>"
+						 height="<?php echo esc_attr( get_custom_header()->height ); ?>"
+						 alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>">
+				</a>
+			</div>
+		<?php endif; ?>
+
 		<!-- Search Button (Both Desktop & Mobile) -->
 		<button
 			id="openSearchPopup"
