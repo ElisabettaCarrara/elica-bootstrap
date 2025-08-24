1b9be92b24643830e0c46546c9062b9b960d44f1
Elisabetta Carrara <146635386+elisabettac77@users.noreply.github.com>
2025-08-24 19:00:58 +0200

    version bump
    version bump

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@ if ( ! function_exists( 'elica_bootstrap_enqueue_scripts' ) ) {
 	 */
 	function elica_bootstrap_enqueue_scripts() {
 		$theme_uri = get_template_directory_uri();
-		$version   = '1.0.0';
+		$version   = '1.1.0';
 
 		// ---------------------------
 		// Enqueue Stylesheets
